6bd724154d1f435664fbf7ff3b8266426ffce4c2
andrewptjio <andrewptjioo@gmail.com>
2022-01-25 15:25:32 +0800

    feature repo,controller and route

--- delivery/routes/features.go
+++ delivery/routes/features.go
@@ -1,1 +1,11 @@
 package routes
+
+import (
+	"github.com/furqonzt99/airbnb/delivery/controllers/feature"
+	"github.com/labstack/echo/v4"
+)
+
+func RegisterFeaturePath(e *echo.Echo, featureCtrl *feature.FeatureController) {
+
+	e.GET("/features", featureCtrl.GetAllFeatureCtrl())
+}
