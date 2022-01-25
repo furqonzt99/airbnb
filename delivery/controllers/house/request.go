e3c7bf3c451037f87ff23aa36a853e3f618448fd
andrewptjio <andrewptjioo@gmail.com>
2022-01-25 14:44:25 +0800

    house repo and controller, open api fix

---
+++ delivery/controllers/house/request.go
@@ -0,0 +1,37 @@
+package house
+
+import (
+	"net/http"
+
+	"github.com/furqonzt99/airbnb/model"
+	"github.com/go-playground/validator/v10"
+	"github.com/labstack/echo/v4"
+)
+
+type CreateHouseRequestFormat struct {
+	Title    string          `json:"title" form:"title" validate:"required"`
+	Address  string          `json:"address" form:"address" validate:"required"`
+	City     string          `json:"city" form:"city" validate:"required"`
+	Price    float64         `json:"price" form:"price" validate:"required"`
+	Features []model.Feature `json:"features" form:"features" validate:"required"`
+}
+
+type PutHouseRequestFormat struct {
+	Title    string          `json:"title" form:"title" validate:"required"`
+	Address  string          `json:"address" form:"address" validate:"required"`
+	City     string          `json:"city" form:"city" validate:"required"`
+	Price    float64         `json:"price" form:"price" validate:"required"`
+	Features []model.Feature `json:"features" form:"features" validate:"required"`
+}
+
+type HouseValidator struct {
+	Validator *validator.Validate
+}
+
+func (cv *HouseValidator) Validate(i interface{}) error {
+	if err := cv.Validator.Struct(i); err != nil {
+		// Optionally, you could return the error to give each route more control over the status code
+		return echo.NewHTTPError(http.StatusBadRequest, err.Error())
+	}
+	return nil
+}
---
+++ repository/house/interface.go
@@ -0,0 +1,11 @@
+package house
+
+import "github.com/furqonzt99/airbnb/model"
+
+type HouseInterface interface {
+	Create(newHouse model.House) (model.House, error)
+	GetAll(offset, pageSize int, search string) ([]model.House, error)
+	Get(houseId int) (model.House, error)
+	Update(newHouse model.House, houseId int) (model.House, error)
+	Delete(houseId int) (model.House, error)
+}
