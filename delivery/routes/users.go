5b6dc7eae3c93025da9ef81d5bd69dc9c76bf28d
andrewptjio <andrewptjioo@gmail.com>
2022-01-25 14:56:06 +0800

    small fix on routes naming for get user profile

--- delivery/routes/users.go
+++ delivery/routes/users.go
@@ -11,7 +11,7 @@ func RegisterUserPath(e *echo.Echo, userCtrl *user.UsersController) {
 
 	e.POST("/register", userCtrl.RegisterController())
 	e.POST("/login", userCtrl.LoginController())
-	e.GET("/users", userCtrl.GetUserController(), middleware.JWT([]byte(constant.JWT_SECRET_KEY)))
+	e.GET("/profile", userCtrl.GetUserController(), middleware.JWT([]byte(constant.JWT_SECRET_KEY)))
 	e.PUT("/users", userCtrl.UpdateUserController(), middleware.JWT([]byte(constant.JWT_SECRET_KEY)))
 	e.DELETE("/users", userCtrl.DeleteUserCtrl(), middleware.JWT([]byte(constant.JWT_SECRET_KEY)))
 }
