dd24f31b00ce2a961376f1f97eb25aa4d74d6d9f
Muhammad Arif Furqon <furqonzt99@gmail.com>
2022-01-26 19:33:26 +0700

    add rating routes

--- delivery/routes/ratings.go
+++ delivery/routes/ratings.go
@@ -1,1 +1,15 @@
 package routes
+
+import (
+	"github.com/furqonzt99/airbnb/constant"
+	"github.com/furqonzt99/airbnb/delivery/controllers/rating"
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+)
+
+func RegisterRatingPath(e *echo.Echo, RatingController *rating.RatingController) {
+	
+	e.POST("/ratings", RatingController.Create, middleware.JWT([]byte(constant.JWT_SECRET_KEY)))
+	e.PUT("/ratings/:productId", RatingController.Update, middleware.JWT([]byte(constant.JWT_SECRET_KEY)))
+	e.DELETE("/ratings/:productId", RatingController.Delete, middleware.JWT([]byte(constant.JWT_SECRET_KEY)))
+}
