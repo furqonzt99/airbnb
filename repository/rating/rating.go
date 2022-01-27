f667e65e59c959ace5a176006428954ce500ea41
andrewptjio <andrewptjioo@gmail.com>
2022-01-27 16:06:47 +0800

    repo rating small fix and test repo 100%

--- repository/rating/rating.go
+++ repository/rating/rating.go
@@ -20,9 +20,7 @@ func (rr RatingRepository) Create(rating model.Rating) (model.Rating, error) {
 
 	var r model.Rating
 
-	if err := rr.db.Preload("User").First(&r, "user_id = ? AND house_id = ?", &rating.UserID, &rating.HouseID).Error; err != nil {
-		return r, err
-	}
+	rr.db.Preload("User").First(&r, "user_id = ? AND house_id = ?", &rating.UserID, &rating.HouseID)
 
 	return r, nil
 }
@@ -30,22 +28,20 @@ func (rr RatingRepository) Create(rating model.Rating) (model.Rating, error) {
 func (rr *RatingRepository) Update(rating model.Rating) (model.Rating, error) {
 	var r model.Rating
 
-	if err := rr.db.First(&r, "user_id = ? AND house_id", rating.HouseID, rating.UserID).Error; err != nil {
+	if err := rr.db.First(&r, "user_id = ? AND house_id = ?", rating.UserID, rating.HouseID).Error; err != nil {
 		return r, err
 	}
 
 	rr.db.Model(&r).Updates(rating)
 
-	if err := rr.db.Preload("User").First(&r, "user_id = ? AND house_id = ?", &rating.UserID, &rating.HouseID).Error; err != nil {
-		return r, err
-	}
+	rr.db.Preload("User").First(&r, "user_id = ? AND house_id = ?", &rating.UserID, &rating.HouseID)
 
 	return r, nil
 }
 
 func (rr *RatingRepository) Delete(userId, houseId int) (model.Rating, error) {
 	rating := model.Rating{}
-	
+
 	if err := rr.db.First(&rating, "user_id = ? AND house_id = ?", userId, houseId).Error; err != nil {
 		return rating, err
 	}
