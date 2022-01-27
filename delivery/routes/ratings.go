6479b94bb5e7fb6642ea46d5663edfc564fd37fe
andrewptjio <andrewptjioo@gmail.com>
2022-01-27 16:55:34 +0800

    unit test for ratings

--- delivery/routes/ratings.go
+++ delivery/routes/ratings.go
@@ -8,8 +8,8 @@ import (
 )
 
 func RegisterRatingPath(e *echo.Echo, RatingController *rating.RatingController) {
-	
+
 	e.POST("/ratings", RatingController.Create, middleware.JWT([]byte(constant.JWT_SECRET_KEY)))
-	e.PUT("/ratings/:productId", RatingController.Update, middleware.JWT([]byte(constant.JWT_SECRET_KEY)))
-	e.DELETE("/ratings/:productId", RatingController.Delete, middleware.JWT([]byte(constant.JWT_SECRET_KEY)))
+	e.PUT("/ratings/:houseId", RatingController.Update, middleware.JWT([]byte(constant.JWT_SECRET_KEY)))
+	e.DELETE("/ratings/:houseId", RatingController.Delete, middleware.JWT([]byte(constant.JWT_SECRET_KEY)))
 }
